ce188c9ab0d7798fcf914d00727298ec4fca8c30
Nikita Samoilov <nikisamoylov@gmail.com>
2022-12-02 21:38:17 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int rubles = 14567;
+        int change = 20;
+        int miles = rubles / change;
+
+        System.out.println("Количество миль за билет: " + miles);
+    }
+}
